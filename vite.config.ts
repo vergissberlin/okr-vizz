7102c1fb381d1d97e3f5630219f2e088b2a6d84a
André Lademann <alademann@thinkport.digital>
2025-06-16 10:26:59 +0200

    build(static.yml): set NODE_ENV to production during build to ensure production optimizations
    feat(package.json): add preview script for serving the dist folder
    
    chore(package.json): add serve package as a dependency for preview script
    
    feat(vite.config.ts): set base path dynamically based on environment to support different deployment contexts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 
+// Bestimme den base-Pfad basierend auf der Umgebung
+const base = process.env.NODE_ENV === "production" ? "/okr-vizz/" : "./";
+
 export default defineConfig({
   plugins: [solid()],
+  base,
   server: {
     port: 3000,
     host: true,
-    strictPort: true
+    strictPort: true,
   },
   build: {
     target: "esnext",
