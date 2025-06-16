5eaf0e208ed25f7724239307baed911e951121d7
André Lademann <alademann@thinkport.digital>
2025-06-16 10:29:08 +0200

    ci(static.yml): set GITHUB_PAGES environment variable to true for build step to ensure correct base path configuration
    fix(vite.config.ts): adjust base path logic to differentiate between GitHub Pages and local development environments

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 
 // Bestimme den base-Pfad basierend auf der Umgebung
-const base = process.env.NODE_ENV === "production" ? "/okr-vizz/" : "./";
+const base = process.env.NODE_ENV === "production" && process.env.GITHUB_PAGES 
+  ? "/okr-vizz/"  // Nur für GitHub Pages
+  : "/";          // Für lokale Entwicklung und Preview
 
 export default defineConfig({
   plugins: [solid()],
